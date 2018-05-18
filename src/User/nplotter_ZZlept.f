eb917afbf59e50a1d79cce2abf208a7b7705cad6
Markus Christian Schulze <markus.schulze@cern.ch>
2018-05-18 12:09:21 +0200

    Added MELA discriminant for Sig+Bkg

--- src/User/nplotter_ZZlept.f
+++ src/User/nplotter_ZZlept.f
@@ -23,7 +23,7 @@ c---                1  --> counterterm for real radiation
       include 'histo.f'
       include 'outputflags.f'
       include 'interference.f'
-      real(dp):: p(mxpart,4),wt,wt2,m3456,pt34,pttwo
+      real(dp):: p(mxpart,4),wt,wt2,m3456,pt34,pttwo,D_MELA
       integer:: switch,n,nplotmax
       integer tag
       logical, save::first=.true.
@@ -35,6 +35,8 @@ ccccc!$omp threadprivate(first,/nplotmax/)
 *     INITIAL BOOKKEEPING                                              *
 *                                                                      *
 ************************************************************************
+      
+    
       
       if (first) then
 c--- Initialize histograms, without computing any quantities; instead
@@ -114,7 +116,7 @@ c--- End Modification -- AP
       call bookplot(n,tag,'130 < m(3456) < 2010',
      & m3456,wt,wt2,130._dp,2010._dp,20._dp,'log')
       n=n+1
-      
+           
       call bookplot(n,tag,'300 < m(3456) < 2020',
      & m3456,wt,wt2,300._dp,2020._dp,20._dp,'log')
       n=n+1
@@ -134,6 +136,25 @@ c--- End Modification -- AP
       call bookplot(n,tag,'50 < m(3456) < 250',
      & m3456,wt,wt2,50._dp,250._dp,2._dp,'log')
       n=n+1
+      
+      
+      
+      call get_MELA_Discr_ggZZ(p,D_MELA)
+      call bookplot(n,tag,'D_MELA(Sgg vs. Bgg)',
+     & D_MELA,wt,wt2,0._dp,1._dp,0.005_dp,'lin')
+      n=n+1
+
+      
+            
+      call get_MELA_Discr_ppZZ(p,D_MELA)
+      call bookplot(n,tag,'D_MELA(Spp vs. Bpp)',
+     & D_MELA,wt,wt2,0._dp,1._dp,0.005_dp,'lin')
+      n=n+1
+
+        
+        
+        
+        
       
 c--- usual plots for 3+4
       call autoplot2(p,34,3,4,tag,wt,wt2,n)
@@ -179,3 +200,85 @@ c--- Set the maximum number of plots, on the first call
       
       return
       end
+
+      
+      
+      
+      subroutine get_MELA_Discr_ggZZ(p,D_MELA)
+      implicit none
+      include 'types.f'
+      include 'mxpart.f'      
+      real(dp):: p(mxpart,4),D_MELA      
+      real(dp) :: msq_SIGINT(-5:5,-5:5),msq_SIG(-5:5,-5:5),msq_BKG
+      
+        msq_SIG(:,:)=0._dp    
+        msq_SIGINT(:,:)=0._dp      
+        msq_BKG=0._dp   
+        
+               
+        call gg_hZZ_tb(p,msq_SIG)      ! this is the signal ME (gg->H->ZZ)
+        call gg_zz(p,msq_BKG)          ! this is the gg-bkg ME (gg->ZZ)
+!         call gg_zz_Hpi(p,msq_SIGINT) ! this includes the interf. term (hence not positive definit)
+
+        D_MELA = msq_SIG(0,0)/(msq_SIG(0,0)+msq_BKG)      
+!         D_MELA = msq_SIGINT(0,0)/(msq_SIGINT(0,0)+msq_BKG)
+
+      
+      return
+      end
+      
+      
+
+      subroutine get_MELA_Discr_PPZZ(p,D_MELA)
+      implicit none
+      include 'types.f'
+      include 'energy.f'      
+      include 'mxpart.f'  
+      include 'nf.f'
+      include 'facscale.f'            
+      include 'nflav.f'      
+      real(dp):: p(mxpart,4),D_MELA      
+      real(dp) :: msq_SIG(-5:5,-5:5),msq_BKGg,msq_BKGq(-5:5,-5:5)
+      real(dp) :: fx1(-nf:nf),fx2(-nf:nf),xmsqjk,xx(1:2)
+      real(dp) :: Collider_Energy,Etot,Pztot
+      integer:: ih1,ih2,j,k
+      common/density/ih1,ih2
+      
+        msq_SIG(:,:)=0._dp    
+        msq_BKGg=0._dp
+        msq_BKGq(:,:) = 0d0
+        
+               
+        call gg_hZZ_tb(p,msq_SIG)      ! this is the signal ME (gg->H->ZZ)
+        call gg_zz(p,msq_BKGg)         ! this is the gg-bkg ME (gg->ZZ)
+        call qqb_zz(p,msq_BKGq)        ! this is the qq-bkg ME (qqb->ZZ)
+
+        
+        Collider_Energy = sqrts
+        Etot = p(3,4)+p(4,4)+p(5,4)+p(6,4)
+        Pztot= p(3,3)+p(4,3)+p(5,3)+p(6,3)
+        xx(1) = (Etot+Pztot)/Collider_Energy
+        xx(2) = (Etot-Pztot)/Collider_Energy
+        
+        call fdist(ih1,xx(1),facscale,fx1)
+        call fdist(ih2,xx(2),facscale,fx2)
+
+        msq_SIG(0,0) = fx1(0)*fx2(0) * msq_SIG(0,0)
+        msq_BKGg     = fx1(0)*fx2(0) * msq_BKGg
+
+        xmsqjk = 0d0
+        do j=-nflav,nflav
+        do k=-nflav,nflav                
+          xmsqjk = xmsqjk + fx1(j)*fx2(k) * msq_BKGq(j,k)
+        enddo 
+        enddo
+       
+        
+        D_MELA = msq_SIG(0,0)/(msq_SIG(0,0)+msq_BKGg+xmsqjk)      
+
+      
+      return
+      end
+            
+      
+      
