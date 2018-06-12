9c67d509663198bc2cb9e67046344bf4466ba81b
Ayan Paul <apaul2@alumni.nd.edu>
2018-06-12 20:55:23 +0200

    fixed bug in previous commit

--- src/User/nplotter_ZZlept.f
+++ src/User/nplotter_ZZlept.f
@@ -130,8 +130,8 @@ c      n=n+1
      & D_MELA,wt,wt2,0._dp,1._dp,0.05_dp,'lin')
       n=n+1
       
-      call get_MELA_Discr_ppZZ_SM(p,D_MELA,1._dp,0._dp)
-      call bookplot(n,tag,'D_PP_BSM_sqrt10_sqrt10(Spp vs. Bpp)',
+      call get_MELA_Discr_ppZZ_BSM(p,D_MELA,1._dp,0._dp)
+      call bookplot(n,tag,'D_PP_SM(Spp vs. Bpp)',
      & D_MELA,wt,wt2,0._dp,1._dp,0.05_dp,'lin')
       n=n+1      
 
